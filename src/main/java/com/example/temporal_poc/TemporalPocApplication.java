284a3d12e29fc58cfc9370293e83a06c3436bd7a
Hashwanth S <hashwanths@Hashwanths-MacBook-Pro.local>
2025-12-23 21:20:56 +0530

    Initial commit

---
+++ src/main/java/com/example/temporal_poc/TemporalPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.temporal_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/temporal_poc/TemporalPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temporal_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
